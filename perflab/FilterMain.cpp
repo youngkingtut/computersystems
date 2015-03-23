b0a7d6599db006b55354408384b19076bf76592a
Tristan <tristanstorz@gmail.com>
2015-03-22 21:57:57 -0600

    inlining funciton calls to filter (getDivisor and getSize). perf03 reporting a CPE of 837

--- perflab/FilterMain.cpp
+++ perflab/FilterMain.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 #include "rtdsc.h"
 
+
+inline int Filter::getDivisor()
+{
+  return divisor;
+}
+
+inline int Filter::getSize()
+{
+  return dim;
+}
+
 //
 // Forward declare the functions
 //
@@ -105,22 +116,21 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
       for(int plane = 0; plane < 3; plane++) {
 
         int t = 0;
-	      output -> color[plane][row][col] = 0;
+	      output->color[plane][row][col] = 0;
 
-	      for (int j = 0; j < filter -> getSize(); j++) {
-          for (int i = 0; i < filter -> getSize(); i++) { 
+	      for (int j = 0; j < filter->getSize(); j++) {
+          for (int i = 0; i < filter->getSize(); i++) { 
 	          output->color[plane][row][col] = output -> color[plane][row][col]
 	                                         + (input -> color[plane][row + i - 1][col + j - 1] * filter->get(i, j) );
 	        }
 	      }
 	
-	      output -> color[plane][row][col] = 	
-	      output -> color[plane][row][col] / filter -> getDivisor();
-	      if ( output -> color[plane][row][col]  < 0 ) {
-	        output -> color[plane][row][col] = 0;
+	      output -> color[plane][row][col] = 	output->color[plane][row][col] / filter->getDivisor();
+	      if (output->color[plane][row][col] < 0 ){
+	        output->color[plane][row][col] = 0;
 	      }
 
-	      if ( output -> color[plane][row][col]  > 255 ) { 
+	      if (output->color[plane][row][col]  > 255){ 
 	        output -> color[plane][row][col] = 255;
 	      }
       }
