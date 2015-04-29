cd89747c13038fa2274386994f0d54fa0402a01d
Tristan Storz <tristanstorz@gmail.com>
2015-04-29 00:32:35 -0600

    word for word copy of the implicit list memory allocation utility provided by the book. Gives a base score of 73.

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -120,7 +120,7 @@ static inline void* PREV_BLKP(void *bp){
 // Global Variables
 //
 
-static char *heap_listp;  /* pointer to first block */  
+static char *heap_listp;  /* pointer to first block */
 
 //
 // function prototypes for internal helper routines
@@ -137,9 +137,17 @@ static void checkblock(void *bp);
 //
 int mm_init(void) 
 {
-  //
-  // You need to provide this
-  //
+  if((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+    return -1;
+  PUT(heap_listp, 0);
+  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+  heap_listp += (2 * WSIZE);
+
+  if(extend_heap(CHUNKSIZE / WSIZE) == NULL)
+    return -1;
+
   return 0;
 }
 
@@ -149,10 +157,18 @@ int mm_init(void)
 //
 static void *extend_heap(size_t words) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+  char *bp;
+  size_t size;
+
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  if((long)(bp = mem_sbrk(size)) == -1)
+    return NULL;
+
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+  PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+  return coalesce(bp);
 }
 
 
@@ -163,6 +179,13 @@ static void *extend_heap(size_t words)
 //
 static void *find_fit(size_t asize)
 {
+  void *bp;
+
+  for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+    if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+      return bp;
+    }
+  }
   return NULL; /* no fit */
 }
 
@@ -171,9 +194,11 @@ static void *find_fit(size_t asize)
 //
 void mm_free(void *bp)
 {
-  //
-  // You need to provide this
-  //
+  size_t size = GET_SIZE(HDRP(bp));
+
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+  coalesce(bp);
 }
 
 //
@@ -181,6 +206,30 @@ void mm_free(void *bp)
 //
 static void *coalesce(void *bp) 
 {
+  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+  if(prev_alloc && next_alloc){ 
+    return bp;
+  }
+  else if(prev_alloc && !next_alloc){ 
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size,0));
+  }
+  else if(!prev_alloc && next_alloc){ 
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  else{
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
   return bp;
 }
 
@@ -189,10 +238,29 @@ static void *coalesce(void *bp)
 //
 void *mm_malloc(size_t size) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+  size_t asize;
+  size_t extendsize;
+  char *bp;
+
+  if(size == 0)
+    return NULL;
+
+  if(size <= DSIZE)
+    asize = 2 * DSIZE;
+  else 
+    asize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+
+  if((bp = find_fit(asize)) != NULL){
+    place(bp, asize);
+    return bp;
+  }
+
+  extendsize = MAX(asize, CHUNKSIZE);
+  if((bp = extend_heap(extendsize / WSIZE)) == NULL)
+    return NULL;
+
+  place(bp, asize);
+  return bp;
 } 
 
 //
@@ -204,6 +272,19 @@ void *mm_malloc(size_t size)
 //
 static void place(void *bp, size_t asize)
 {
+  size_t csize = GET_SIZE(HDRP(bp));
+
+  if((csize - asize) >= (2 * DSIZE)){
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
+    bp = NEXT_BLKP(bp);
+    PUT(HDRP(bp), PACK(csize-asize, 0));
+    PUT(FTRP(bp), PACK(csize-asize, 0));
+  }
+  else{
+    PUT(HDRP(bp), PACK(csize, 1));
+    PUT(FTRP(bp), PACK(csize, 1));
+  }
 }
 
 
