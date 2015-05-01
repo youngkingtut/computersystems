f726143de485d51396ff6ad64594008f97c6f0ee
Tristan Storz <tristanstorz@gmail.com>
2015-05-01 09:36:26 -0600

    Singlely linked free list now in use. Average improved to 75. Grade is 76.

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -214,7 +214,6 @@ int mm_init(void)
   if((bp = extend_heap(CHUNKSIZE / WSIZE)) == NULL)
     return -1;
 
-  APPEND(bp);
   return 0;
 }
 
@@ -246,13 +245,16 @@ static void *extend_heap(size_t words)
 //
 static void *find_fit(size_t asize)
 {
-  void *bp;
+  void * bp = free_head;
 
-  for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+  while(bp != NULL){
     if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
       return bp;
+    } else{
+      bp = (void *)GET(bp);
     }
   }
+
   return NULL; /* no fit */
 }
 
@@ -278,21 +280,26 @@ static void *coalesce(void *bp)
   size_t size = GET_SIZE(HDRP(bp));
 
   if(prev_alloc && next_alloc){ 
+    APPEND(bp);
     return bp;
   }
   else if(prev_alloc && !next_alloc){ 
     size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    DELETE(NEXT_BLKP(bp));
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size,0));
   }
   else if(!prev_alloc && next_alloc){ 
     size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    DELETE(PREV_BLKP(bp));
     PUT(FTRP(bp), PACK(size, 0));
     PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
     bp = PREV_BLKP(bp);
   }
   else{
     size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    DELETE(NEXT_BLKP(bp));
+    DELETE(PREV_BLKP(bp));
     PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
     PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
     bp = PREV_BLKP(bp);
@@ -344,12 +351,14 @@ static void place(void *bp, size_t asize)
 {
   size_t csize = GET_SIZE(HDRP(bp));
 
+  DELETE(bp);
   if((csize - asize) >= (2 * DSIZE)){
     PUT(HDRP(bp), PACK(asize, 1));
     PUT(FTRP(bp), PACK(asize, 1));
     bp = NEXT_BLKP(bp);
     PUT(HDRP(bp), PACK(csize-asize, 0));
     PUT(FTRP(bp), PACK(csize-asize, 0));
+    APPEND(bp);
   }
   else{
     PUT(HDRP(bp), PACK(csize, 1));
